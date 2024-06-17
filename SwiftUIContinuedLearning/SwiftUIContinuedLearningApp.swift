a3f93f8f654b5c5fd7d38f0aad9cdda757075aad
Saksham <saksham.23bce10006@vitbhopal.ac.in>
2024-06-17 08:04:43 +0530

    Initial Commit

---
+++ SwiftUIContinuedLearning/SwiftUIContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContinuedLearningApp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by Saksham Gupta on 17/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
