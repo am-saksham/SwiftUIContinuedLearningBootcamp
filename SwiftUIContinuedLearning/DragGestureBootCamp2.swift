b74c3e8464194e7582789d39ae0ad1051d5def9b
Saksham <saksham.23bce10006@vitbhopal.ac.in>
2024-06-19 07:39:09 +0530

    Drag Gesture BootCamp

---
+++ SwiftUIContinuedLearning/DragGestureBootCamp2.swift
@@ -0,0 +1,87 @@
+//
+//  DragGestureBootCamp2.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by Saksham Gupta on 19/06/24.
+//
+
+import SwiftUI
+
+struct DragGestureBootCamp2: View {
+    
+    @State var startingOffsetY: CGFloat = UIScreen.main.bounds.height * 0.85
+    @State var currentDragOffsetY: CGFloat = 0
+    @State var endingOffsetY: CGFloat = 0
+    
+    var body: some View {
+        ZStack {
+            Color.green.ignoresSafeArea()
+            
+            MySignUpView()
+                .offset(y: startingOffsetY)
+                .offset(y: currentDragOffsetY)
+                .offset(y: endingOffsetY)
+                .gesture(
+                    DragGesture()
+                        .onChanged({ value in
+                            withAnimation(.spring()) {
+                                currentDragOffsetY = value.translation.height
+                            }
+                        })
+                        .onEnded({ value in
+                            
+                            withAnimation(.spring()) {
+                                if currentDragOffsetY < -150 {
+                                    endingOffsetY = -startingOffsetY
+                                    currentDragOffsetY = 0
+                                } else if endingOffsetY != 0 && currentDragOffsetY > 150 {
+                                    endingOffsetY = 0
+                                    currentDragOffsetY = 0
+                                }
+                                else {
+                                    currentDragOffsetY = 0
+                                }
+                            }
+                        })
+                )
+            Text("\(currentDragOffsetY)")
+        }
+    }
+}
+
+#Preview {
+    DragGestureBootCamp2()
+}
+
+struct MySignUpView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Image(systemName: "chevron.up")
+                .padding(.top)
+            Text("Sign Up")
+                .font(.headline)
+                .fontWeight(.semibold)
+            
+            Image(systemName: "flame.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+            
+            Text("This is the description of my app. This is my favorite SwiftUI course and I recommend to all of my friends to subscribe to Swiftful Thinking.")
+                .multilineTextAlignment(.center)
+            
+            Text("CREATE AN ACCOUNT")
+                .foregroundColor(.white)
+                .font(.headline)
+                .padding()
+                .padding(.horizontal)
+                .background(Color.black)
+                .cornerRadius(10)
+            Spacer()
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+        .ignoresSafeArea(edges: .bottom)
+    }
+}
